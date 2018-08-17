7b836de2e8d7607bcbe5f5ae9094b063f1fd1635
darla <space-age-pete@users.noreply.github.com>
2018-08-17 10:48:49 -0500

    fixed problem with zip code authenticating function

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,8 @@
-function isZip(zip) {
-    return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zip);
-}
-
-
-
-
 $("#zipButton").on("click", function () {
     preventDefault();
     var zipCode = $("#zipBox").val();
-    if (isZip(zipCode)) {
+    if (/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode)) {
         // Do search function
+        console.log("That is a valid Zip Code")
     }
 })
